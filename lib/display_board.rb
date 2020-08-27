0069ac5178c43e9af99708c8c92a6a1d0f4aac3a
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-08-27 18:09:18 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,13 +2,10 @@
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def display_board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-
 def display_board(board)
-  puts 
+  rows puts "   |   |   "
+  rows puts "-----------"
+  rows puts "   |   |   "
+  rows puts "-----------"
+  rows puts "   |   |   "
+end
