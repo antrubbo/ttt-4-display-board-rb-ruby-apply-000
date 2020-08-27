2b16a59e4ac20331f193d4389dd4418d2e586525
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-08-27 17:58:22 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" ", " ", " " [" ", " ", " "], row3 = [" ", " ", " "])
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
