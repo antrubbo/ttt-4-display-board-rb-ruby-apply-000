0df831773570965c2f51f96055de1bd7fa2d66e7
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-08-27 18:32:40 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,9 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
-  rows[1] = puts "   |   |   "
-  rows[2] = puts "-----------"
-  rows[3] = puts "   |   |   "
-  rows[4] = puts "-----------"
-  rows[5] = puts "   |   |   "
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
